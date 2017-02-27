fd464d9fb32624d4f28de8541ba34be702808ef3
Иван Лозинский < >
2017-02-27 11:47:54 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <time.h>
+// long long - mod - boble - heap
+// 10 - 100 - 1000 - 10000
+// ordered - back ordered - random x2
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
